9c2da6282220a73200b4070fcd8dd98922ff96bb
aseladk <38081311+AselaDK@users.noreply.github.com>
2019-10-20 23:40:56 +0530

    all are done... has to comment inside....

---
+++ firstProgramme.R
@@ -0,0 +1,2 @@
+print("Hello World")
+print("I am going to learn OCTAVE!")
---
+++ secondProgramme.R
@@ -0,0 +1,12 @@
+A <- 105
+B <- 9.5 * 10^35
+C <- 7 + 5i
+D <- 4 - 2i
+E <- C - D
+aVec <- c(5.3, 3.4, 6, 23)
+bVec <- matrix(c(5.3, 3.5, 6, 100),nrow=4)
+cVec <- seq(5,-5,length=21)
+dVec <- 'University of Colombo School of Computing'
+dVec <- c('University of Colombo School of Computing')
+dVec <- as.character('University of Colombo School of Computing')
+dVec <- strsplit('University of Colombo School of Computing','')
